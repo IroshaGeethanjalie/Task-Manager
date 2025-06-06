3920adc208ce3602d63fbae7d8e77f98050722f9
geeth <111299707+IroshaGeethanjalie@users.noreply.github.com>
2025-06-06 14:11:04 +0530

    created AuthLayout.jsx

---
+++ frontend/Task-Manager/src/components/layouts/AuthLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UI_IMG from "../../assets/images/auth-img.jpeg";
+
+const AuthLayout = ({children}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default AuthLayout;
